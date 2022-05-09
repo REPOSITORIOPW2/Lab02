fd2e1af50053797b32d06ef831a05dae3e1f925f
kevinPedroYare <kyare@unsa.edu.pe>
2022-05-09 17:21:24 -0500

    Se cambio el tipo de archivo del ejercicio 3 de .js a .html y se modifico para que funcione como una pagina

--- Ejercicio3.js
+++
@@ -1,6 +0,0 @@
-function DiasFaltantes(){
-    var fecha = new Date(2022, 07, 15);
-    var hoy = new Date();
-    var restante = fecha - hoy;
-    return "Faltan : " + parseInt((restante/86400000)+1) + " días";
-}
